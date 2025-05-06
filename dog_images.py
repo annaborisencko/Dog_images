59b27745d0cabacfc5967b836c25951cdef905b3
Anna Sidorenko <annaborisencko@yandex.ru>
2025-05-06 16:14:47 +0300

    Add tqdm progress-bar and error_logs

--- dog_images.py
+++ dog_images.py
@@ -1,5 +1,9 @@
 import requests
 import json
+import csv
+import time
+import os
+from tqdm import tqdm
 
 #Параметры для доступа к API
 DOG_URL = r'https://dog.ceo/api/breed/'
@@ -25,12 +29,15 @@ def get_existing_files(breed):
         response = requests.get(YA_URL, headers=headers, params = params)
         if response.status_code == 200:
             limit = response.json()['_embedded']['total']
-            params.update({'limit': limit})
-            response = requests.get(YA_URL, headers=headers, params = params)
-            file_info_list = response.json()['_embedded']['items']
-            for file in file_info_list:
-                file_list.append(file['name'])
-            return file_list, f"Получен список ранее загруженных файлов, всего в папке сейчас хранится {limit} файлов. Повторная загрузка ранее загруженных файлов не требуется."
+            if limit > 0:
+                params.update({'limit': limit})
+                response = requests.get(YA_URL, headers=headers, params = params)
+                file_info_list = response.json()['_embedded']['items']
+                for file in file_info_list:
+                    file_list.append(file['name'])
+                return file_list, f"Получен список ранее загруженных файлов, всего в папке сейчас хранится {limit} файлов. Повторная загрузка ранее загруженных файлов не требуется."
+            else:
+                return True, f"Папка {breed} пока не содержит файлов"
         else:
             error_data = response.json()
             error_msg = error_data.get('message', 'Неизвестная ошибка')
@@ -89,25 +96,33 @@ def upload_images_to_YA_DISK(breed):
         return folder_message
     print(folder_message)
 
+    current_date = time.strftime("%d.%m.%Y", time.localtime())
+    current_time = time.strftime("%H:%M:S", time.localtime())
     upload_list = []
     count = 0
+    errors = [['Дата', 'Наименование файла', 'Описание ошибки']]
     existing_files_count = 0
+    
     existing_files, existing_files_message = get_existing_files(en_breed)
     if not existing_files:
         return existing_files_message
     print(existing_files_message)
-    errors = []
-    for image in sub_breed_images_list:
-        
-        file_name = f"{en_breed}_{image.split('/')[-1]}"
+    
+    # Главный прогресс-бар
+    with tqdm(total=total_image_count, desc=f"Статистика загрузки изображений({breed})", unit="файл", position=0) as progress_bar_total:
+        for image in sub_breed_images_list:
+            
+            file_name = f"{en_breed}_{image.split('/')[-1]}"
 
-        #Пропускаем ранее загруженные файлы
-        if file_name in existing_files:
-            #print(f"Файл {file_name} был загружен на Яндекс.Диск ранее. Повторная загрузка не требуется")
-            existing_files_count += 1
-            upload_list.append({'file_name': file_name})
-            continue
-        else:
+            #Пропускаем ранее загруженные файлы
+            if type(existing_files) == list and file_name in existing_files:
+                #print(f"Файл {file_name} был загружен на Яндекс.Диск ранее. Повторная загрузка не требуется")
+                existing_files_count += 1
+                upload_list.append({'file_name': file_name})
+                progress_bar_total.write(f"Пропущен: {file_name} (уже существует)")
+                progress_bar_total.update(1)
+                continue
+            
             path = f"{en_breed}/{file_name}"
             headers = get_YA_DISK_headers(YA_TOKEN)
             params = {
@@ -115,39 +130,56 @@ def upload_images_to_YA_DISK(breed):
                 'url': image
             }
             try:
-                operation = requests.post(f"{YA_URL}/upload", headers=headers, params=params, stream=True).json()['href']
+                upload_response = requests.post(f"{YA_URL}/upload", headers=headers, params=params, stream=True)
+                upload_response.raise_for_status()
 
                 #Проверяем статус асинхронной отправки изображения 
-                operation_status_check = requests.get(operation, headers=headers).status_code
+                operation_status_check = requests.get(upload_response.json()['href'], headers=headers).status_code
                 if operation_status_check == 200:
                     count += 1
                     upload_list.append({'file_name': file_name})  
-                    print(f"Файл {file_name} успешно загружен на Яндекс.Диск.")
+                    progress_bar_total.write(f"Успешно загружен: {file_name}")
+                    progress_bar_total.update(1)
                 else:
                     raise Exception(f"HTTP {operation_status_check.status_code}")
             except Exception as exc:
-                    error_msg = f"Ошибка: {file_name} ({str(exc)})"
+                    error_msg = [f"{current_date} {current_time}", file_name, str(exc)]
                     errors.append(error_msg)
+                    progress_bar_total.write(f"Ошибка загрузки: {file_name}")
 
-    #Сохраняем список загруженных изображений в json файл
+    # Сохраняем список загруженных изображений в json файл
     if upload_list:
-        with open('uploaded_files.json', 'w', encoding='utf-8') as f:
+        path_json_file = os.path.join(os.getcwd(), 'uploaded_files.json')
+        with open(path_json_file, 'w', encoding='utf-8') as f:
             json.dump(upload_list,f, indent=2)
 
+    # Сохраняем список файлов, по которым возникла ошибка
+    csv.register_dialect("csv_for_excel", delimiter=";", quoting=csv.QUOTE_NONE, escapechar="\\")
+    if len(errors) > 1:
+        path_error_file = os.path.join(os.getcwd(), 'error_logs', f'error_file_{current_date}.csv')
+        if os.path.isfile(path_error_file):
+            with open(path_error_file, 'a', encoding='utf-8', newline="") as error_file:   
+                writer = csv.writer(error_file, dialect='csv_for_excel')
+                writer.writerows(errors[1:])
+        else:
+             with open(path_error_file, 'w', encoding='utf-8', newline="") as error_file:   
+                writer = csv.writer(error_file, dialect='csv_for_excel')
+                writer.writerows(errors)     
+            
     # Выводим сообщение со статистикой загрузки пользователю
     result = [
         f"\nРезультат обработки:",
         f"Порода: {breed}",
-        f"Всего изображений: {total_image_count}",
+        f"Всего изображений: {total_image_count}. Список загруженных изображений: {f.name}",
         f"Успешно загружены: {count}",
         f"Загружены ранее: {existing_files_count}",
-        f"Ошибки: {len(errors)}"
     ]
-        
+    if len(errors) >  1:
+        result.insert(-1, f"Ошибки: {len(errors)-1}. Просмотреть ошибки: {error_file.name}") 
     return '\n'.join(result)
     
 
-breed = input('Введите наименование породы собак на английском языке: ')
+breed = input('Полный список пород доступен по адресу: https://dog.ceo/dog-api/breeds-list. Введите наименование породы собак на английском языке, например, african: ')
 YA_TOKEN = input('Введите значение OAuth-токена, полученного для работы с API Яндекс.Диска: ')
 
 result = upload_images_to_YA_DISK(breed)
